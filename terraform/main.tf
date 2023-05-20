067910c89ce1c7dc6a838294b6dfcaca92e8534c
Jonathan Boniface <jonny.boniface@waltlabs.io>
2023-05-20 16:11:21 +0100

    Updating workflows and modifing compute disk regions

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,7 @@ locals {
 resource "google_compute_disk" "default" {
   name  = "test-disk"
   type  = "pd-ssd"
-  zone  = "us-central1-a"
+  zone  = "europe-west2-a"
   image = "debian-11-bullseye-v20220719"
   labels = {
     environment = "dev"
@@ -33,7 +33,7 @@ resource "google_compute_disk" "default" {
 resource "google_compute_disk" "default-2" {
   name  = "test-disk-2"
   type  = "pd-ssd"
-  zone  = "us-central1-a"
+  zone  = "europe-west2-a"
   image = "debian-11-bullseye-v20220719"
   labels = {
     environment = "dev"

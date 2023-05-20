804326a8a0b2587886681c75c1b68f00c36e0b2f
Jonathan Boniface <jonny.boniface@waltlabs.io>
2023-05-20 16:04:37 +0100

    Adding workflows to github

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,3 +29,14 @@ resource "google_compute_disk" "default" {
   }
   physical_block_size_bytes = 4096
 }
+
+resource "google_compute_disk" "default-2" {
+  name  = "test-disk-2"
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
+  image = "debian-11-bullseye-v20220719"
+  labels = {
+    environment = "dev"
+  }
+  physical_block_size_bytes = 4096
+}

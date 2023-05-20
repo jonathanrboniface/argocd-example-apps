be21f11beddcd3ba8a53f4edb13d180d93c3927e
Jonathan Boniface <jonny.boniface@waltlabs.io>
2023-05-20 16:25:34 +0100

    Removing compute disks added

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,25 +18,3 @@ locals {
     "data-project" = var.data-project
   }
 }
-
-resource "google_compute_disk" "default" {
-  name  = "test-disk"
-  type  = "pd-ssd"
-  zone  = "europe-west2-a"
-  image = "debian-11-bullseye-v20220719"
-  labels = {
-    environment = "dev"
-  }
-  physical_block_size_bytes = 4096
-}
-
-resource "google_compute_disk" "default-2" {
-  name  = "test-disk-2"
-  type  = "pd-ssd"
-  zone  = "europe-west2-a"
-  image = "debian-11-bullseye-v20220719"
-  labels = {
-    environment = "dev"
-  }
-  physical_block_size_bytes = 4096
-}
